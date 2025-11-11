7d2d9a03c25846edcd314e41152e86172f94d140
krotka <oliwiapolonia@gmail.com>
2025-11-11 17:53:11 +0100

    change terminal mapping to not conflict with text deletion

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -9,5 +9,5 @@ map('n', '<ESC>', '<cmd>nohlsearch<cr>')
 --- navigating the terminal ---
 map('n', '<leader>v', util.open_terminal_split)  -- open a configured terminal to the right
 map('t', '<C-[>', '<C-\\><C-n>')                 -- exit terminal
-map('t', '<C-w>', '<C-\\><C-n><C-w>')            -- change nvim window
+map('t', '<C-q>', '<C-\\><C-n><C-w>')            -- change nvim window
 -- <C-d> to exit (I'm writing this down here so I won't forget it and make a redundant keymap)
