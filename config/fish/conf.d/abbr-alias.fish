8d7c054ea3c36dd14879bd55abbe16314a9f7a41
krotka <oliwiapolonia@gmail.com>
2025-11-22 22:23:38 +0100

    git status abbreviation + promote some abbreviations

--- config/fish/conf.d/abbr-alias.fish
+++ config/fish/conf.d/abbr-alias.fish
@@ -1,7 +1,10 @@
 if status is-interactive
-  # abbreviations I use oftem
+  # abbreviations I use often
   abbr --add ccp wl-copy
   abbr --add cpst wl-paste
+  abbr --add gits git status
+  abbr --add glon git clone --depth 1  # useful for one-time repo browsing
+  abbr --add orphans pacman -Qtdq
   abbr --add pacs sudo pacman
   abbr --add pacwhich pacman -Qo
   abbr --add watch mpv --fs
@@ -9,8 +12,6 @@ if status is-interactive
   abbr --add android aft-mtp-cli
   abbr --add cpv rsync -av --progress
   abbr --add gcd cd \(git rev-parse --show-toplevel\)
-  abbr --add glon git clone --depth 1  # useful for one-time repo browsing
-  abbr --add orphans pacman -Qtdq
   abbr --add usb lfcd /run/media/krotka
 
   alias lf=lfcd
