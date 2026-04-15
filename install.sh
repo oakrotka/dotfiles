c44f4504b751b00d268a5b605db09fa313eac5f9
krotka <oliwiapolonia@gmail.com>
2026-04-15 17:10:11 +0200

    ideavim config

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ try_installfile() {
 ./typst/typst-install.sh ./typst/krommon "0.1.0"
 
 installdir $HOME/.config ./config/fish ./config/nvim ./config/pythonrc ./config/satty
-installdir $HOME/.config ./config/swayidle ./config/fuzzel
+installdir $HOME/.config ./config/swayidle ./config/fuzzel ./config/ideavim
 
 [ -d $HOME/.local/bin ] || mkdir $HOME/.local/bin/
 installdir $HOME/.local/bin ./local/bin/screenshot ./local/bin/strip-prefix ./local/bin/uwusay
